31c5187e3f10f14e1b44754af7762f4814028be8
oluwasegun <pholarwalk@gmail.com>
2019-09-09 17:53:58 +0100

    conflict resolving

--- js/auth.js
+++ js/auth.js
@@ -1,20 +1,23 @@
+
 const signupForm = document.querySelector('#signup-data');
-const signupBtn = document.querySelector('#sign-up');
-signupBtn.addEventListener('click', (e) =>{
+
+// const signupBtn = document.querySelector('#sign-up');
+signupForm.addEventListener('submit', (e) =>{
     e.preventDefault();
 
     // get user info
-    const fname = signupForm['first_name'].value;
-    const lname = signupForm['last_name'].value;
+    // const fname = signupForm['first_name'].value;
+    // const lname = signupForm['last_name'].value;
     const email = signupForm['email'].value;
     const password = signupForm['password'].value;
-    const phone = signupForm['telephone'].value;
-    console.log(email, password, fname, lname, phone);
+    // const phone = signupForm['telephone'].value;
+    console.log(email, password);
+
     // sign up user
     
     auth.createUserWithEmailAndPassword(email , password).then(cred =>{
       console.log(cred);
-    
+     
         signupForm.reset();
     });
 });
