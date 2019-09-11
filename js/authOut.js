620bf9c2fed3cdfaef7e6a0be39953f8e48b8b28
Habib Ogunsola <ogunsolahabib@yahoo.com>
2019-09-11 16:54:28 +0100

    remove modernizer

--- js/authOut.js
+++ js/authOut.js
@@ -1,12 +1,12 @@
 // log out
 
-window.onload = function() {
-  const logout = document.querySelector("#log-out");
-  logout.addEventListener("click", e => {
-    e.preventDefault();
-    auth.signOut().then(() => {
-      console.log("user signed out");
-      window.location = "index.html";
-    });
-  });
-};
+window.onload=function(){
+const logout = document.querySelector('#log-out');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+    window.location='index.html';
+  })
+});
+}
